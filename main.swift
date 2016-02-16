0dbd6c914b0a1e0bfa2332ce9b3cd04b50cd3941
Jonas Wippermann <jonas@vfuc.co>
2016-02-16 18:21:40 +0200

    Reordering functions for better readability

--- main.swift
+++ main.swift
@@ -17,74 +17,7 @@ struct Color {
 }
 
 
-
-func printHelp() {
-	print("Avea-CLI\n")
-
-	print("Usage options:\n")
-
-	print(" avea rgbw [red] [green] [blue] [white]")
-	print(" avea set-color-rgbw [red] [green] [blue] [white]")
-	print("\t\tSet color according to red, green, blue and white value in range of 0-255\n")	
-
-	print(" avea off")
-	print("\t\t Turn avea off\n")
-	
-	print(" avea c [descriptor]")
-	print(" avea set-color [descriptor]")
-	print("\t\tSet color using color descriptor\n")
-
-	print(" avea show-colors")
-	print("\t\t Show all color descriptors\n")
-
-	print(" avea add-color [name] [red] [green] [blue] [white]")
-	print("\t\t Add color descriptor with associated RGBW values\n")
-
-	print(" avea delete-color [name]")
-	print("\t\t Delete a color descriptor\n")
-
-	print(" avea help")
-	print("\t\t Show this help\n")
-
-	print("\n\ngithub.com/vfuc/avea-cli")
-	print("vfuc.co")
-}
-
-
-func setColorUsingRGBW(){
-	guard Process.arguments.count == 6 else	 { // self + command + 4 arguments = 6
-		print("[Error] Wrong number of arguments! Needs [red] [green] [blue] [white]")
-		exit(1)
-	}
-
-	guard let red = Int(Process.arguments[2]) where (0...255).contains(red) else {
-		print("[Error] Red value (\(Process.arguments[2])) is not an Int or out of range (0-255)")
-		exit(1)
-	}
-	
-	guard let green  = Int(Process.arguments[3]) where (0...255).contains(green) else {
-		print("[Error] Green value (\(Process.arguments[3])) is not an Int or out of range (0-255)")
-		exit(1)
-	}
-	
-	guard let blue  = Int(Process.arguments[4]) where (0...255).contains(blue) else {
-		print("[Error] Blue value (\(Process.arguments[4])) is not an Int or out of range (0-255)")
-		exit(1)
-	}
-	
-	guard let white = Int(Process.arguments[5]) where (0...255).contains(white) else {
-		print("[Error] White value (\(Process.arguments[5])) is not an Int or out of range (0-255)")
-		exit(1)
-	}
-	
-	print("[setColor] Red: \(red), Green: \(green), Blue: \(blue), White: \(white)")		
-	Avea().setColor(red: red, green: green, blue: blue, white: white)
-}
-
-func turnOff(){
-	print("[turnOff] Turning off Avea")
-	Avea().setColor(red: 0, green: 0, blue: 0, white: 0)
-}
+/* FUNCTIONS */
 
 func getColorsFromFile() -> [Color]? {
 	guard let data = NSData(contentsOfFile: Constants.ColorDescriptorFile) else {
@@ -131,43 +64,6 @@ func getColorsFromFile() -> [Color]? {
 	}
 }
 
-
-func setColorUsingDescriptor(){
-	guard Process.arguments.count == 3 else	 { // self + command + 1 arguments = 3
-		print("[Error] Wrong number of arguments! See help for usage details")
-		exit(1)
-	}
-	
-	let input = Process.arguments[2]
-
-	guard let colors = getColorsFromFile() else {
-		print("Colors not loaded, exiting")
-		exit(1)
-	}
-
-	for color in colors where color.title == input {
-		print("[setColor] \(input) - Red: \(color.red), Green: \(color.green), Blue: \(color.blue), White: \(color.white)")		
-		Avea().setColor(red: color.red, green: color.green, blue: color.blue, white: color.white)
-		return
-	}
-
-	print("[Error] Color Descriptor not recognized! Show available colors using \'avea show-colors\'") 
-}
-
-
-func showColorDescriptors(){
-	guard let colors = getColorsFromFile() else {
-		print("Colors not loaded, exiting")
-		exit(1)
-	}
-
-	print("Available colors: \n")
-	for color in colors {
-		print("[\(color.title)] Red: \(color.red), Green: \(color.green), Blue: \(color.blue), White: \(color.white)")
-	}
-}
-
-
 func getJSONDataForColors(colors: [Color]) -> NSData? {
 	var colorDicts = [[String : AnyObject]]()
 	
@@ -195,6 +91,89 @@ func getJSONDataForColors(colors: [Color]) -> NSData? {
 }
 
 
+
+
+
+
+
+
+
+/* COMMANDS */
+
+// "rgbw", "set-color-rgbw"
+func setColorUsingRGBW(){
+	guard Process.arguments.count == 6 else	 { // self + command + 4 arguments = 6
+		print("[Error] Wrong number of arguments! Needs [red] [green] [blue] [white]")
+		exit(1)
+	}
+
+	guard let red = Int(Process.arguments[2]) where (0...255).contains(red) else {
+		print("[Error] Red value (\(Process.arguments[2])) is not an Int or out of range (0-255)")
+		exit(1)
+	}
+	
+	guard let green  = Int(Process.arguments[3]) where (0...255).contains(green) else {
+		print("[Error] Green value (\(Process.arguments[3])) is not an Int or out of range (0-255)")
+		exit(1)
+	}
+	
+	guard let blue  = Int(Process.arguments[4]) where (0...255).contains(blue) else {
+		print("[Error] Blue value (\(Process.arguments[4])) is not an Int or out of range (0-255)")
+		exit(1)
+	}
+	
+	guard let white = Int(Process.arguments[5]) where (0...255).contains(white) else {
+		print("[Error] White value (\(Process.arguments[5])) is not an Int or out of range (0-255)")
+		exit(1)
+	}
+	
+	print("[setColor] Red: \(red), Green: \(green), Blue: \(blue), White: \(white)")		
+	Avea().setColor(red: red, green: green, blue: blue, white: white)
+}
+
+// "c", "set-color"
+func setColorUsingDescriptor(){
+	guard Process.arguments.count == 3 else	 { // self + command + 1 arguments = 3
+		print("[Error] Wrong number of arguments! See help for usage details")
+		exit(1)
+	}
+	
+	let input = Process.arguments[2]
+
+	guard let colors = getColorsFromFile() else {
+		print("Colors not loaded, exiting")
+		exit(1)
+	}
+
+	for color in colors where color.title == input {
+		print("[setColor] \(input) - Red: \(color.red), Green: \(color.green), Blue: \(color.blue), White: \(color.white)")		
+		Avea().setColor(red: color.red, green: color.green, blue: color.blue, white: color.white)
+		return
+	}
+
+	print("[Error] Color Descriptor not recognized! Show available colors using \'avea show-colors\'") 
+}
+
+// "off"
+func turnOff(){
+	print("[turnOff] Turning off Avea")
+	Avea().setColor(red: 0, green: 0, blue: 0, white: 0)
+}
+
+// "show-colors"
+func showColorDescriptors(){
+	guard let colors = getColorsFromFile() else {
+		print("Colors not loaded, exiting")
+		exit(1)
+	}
+
+	print("Available colors: \n")
+	for color in colors {
+		print("[\(color.title)] Red: \(color.red), Green: \(color.green), Blue: \(color.blue), White: \(color.white)")
+	}
+}
+
+// "add-color"
 func addColor(){
 	guard Process.arguments.count == 7 else { // self + command + 5 arguments = 7
 		print("[Error] Wrong number of arguments! See help for usage details")
@@ -252,7 +231,7 @@ func addColor(){
 	print("[\(addedColor.title)] added to colors")
 }
 
-
+// "delete-color"
 func deleteColor(){
 	guard Process.arguments.count == 3 else { // self + command + 1 argument = 3
 		print("[Error] Wrong number of arguments! See help for usage details")
@@ -290,6 +269,40 @@ func deleteColor(){
 	fileHandle.writeData(jsonData)
 	print("[\(title)] removed from colors")
 }
+
+// "help"
+func printHelp() {
+	print("Avea-CLI\n")
+
+	print("Usage options:\n")
+
+	print(" avea rgbw [red] [green] [blue] [white]")
+	print(" avea set-color-rgbw [red] [green] [blue] [white]")
+	print("\t\tSet color according to red, green, blue and white value in range of 0-255\n")	
+
+	print(" avea off")
+	print("\t\t Turn avea off\n")
+	
+	print(" avea c [descriptor]")
+	print(" avea set-color [descriptor]")
+	print("\t\tSet color using color descriptor\n")
+
+	print(" avea show-colors")
+	print("\t\t Show all color descriptors\n")
+
+	print(" avea add-color [name] [red] [green] [blue] [white]")
+	print("\t\t Add color descriptor with associated RGBW values\n")
+
+	print(" avea delete-color [name]")
+	print("\t\t Delete a color descriptor\n")
+
+	print(" avea help")
+	print("\t\t Show this help\n")
+
+	print("\n\ngithub.com/vfuc/avea-cli")
+	print("vfuc.co")
+}
+
 
 
 
